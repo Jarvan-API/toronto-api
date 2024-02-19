c14e3029e0257b6b2cb624b1554c5327a2a4e7a1
Mbetania <betaniamarianag@gmail.com>
2024-02-19 11:22:30 -0300

    update

--- src/application/use-cases/character/get-haram.use-case.ts
+++ src/application/use-cases/character/get-haram.use-case.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable, Logger } from "@nestjs/common";
 
 import { PORT } from "src/application/enums";
-import { IHaram } from "src/application/presentations/character.presentations";
 import { ICharacter } from "src/domain/entities";
 import { IUserRepository } from "src/domain/interfaces";
 import { IMarryRepository } from "src/domain/interfaces/character-repository.interfaces";
