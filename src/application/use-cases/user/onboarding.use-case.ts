2d1aa5e90f37f91776060d4773ada7d7b69eb98a
federicoPedraza <federico.pedraza4@gmail.com>
2024-03-13 00:15:39 -0300

    Prepare enviroments

--- src/application/use-cases/user/onboarding.use-case.ts
+++ src/application/use-cases/user/onboarding.use-case.ts
@@ -5,31 +5,21 @@ import { OnboardingDTO } from "src/application/dtos";
 import { EUserStatus, PORT } from "src/application/enums";
 import { OnboardingAlreadyMade, UserNotFound } from "src/application/exceptions";
 import { IUserRepository } from "src/domain/interfaces";
-import { FlagService } from "src/application/services";
 
 @Injectable()
 export class Onboarding {
   private readonly logger = new Logger(Onboarding.name);
 
-  constructor(
-    @Inject(PORT.User) private readonly userRepository: IUserRepository,
-    private readonly flag: FlagService,
-  ) {}
+  constructor(@Inject(PORT.User) private readonly userRepository: IUserRepository) {}
 
   async exec(data: OnboardingDTO, userId: string): Promise<any> {
     const obid = new Types.ObjectId(userId);
-    this.flag.mark("OBID created", { name: "Onboarding" });
 
     const user = await this.userRepository.findOne({ query: { _id: obid } });
-    this.flag.mark("User collected from DB");
 
     if (!Boolean(user)) throw new UserNotFound();
-    this.flag.mark("User found");
-
     if (user.status !== EUserStatus.PENDING_ONBOARDING) throw new OnboardingAlreadyMade();
-    this.flag.mark("User status checked");
 
     await this.userRepository.update(userId, { ...data, status: EUserStatus.PENDING });
-    this.flag.mark("User updated");
   }
 }
--- src/infrastructure/modules/user.module.ts
+++ src/infrastructure/modules/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { HttpModule } from "@nestjs/axios";
 
 import { Entity, PORT } from "src/application/enums";
 import { ChangeUserStatus, ChangeProfilePicture, GetAuthSession, GetUserProfile, ListPendingUsers, Onboarding, RequestRecovery, ChangePassword } from "src/application/use-cases";
 import { AdminLogSchema, SessionSchema, UserSchema, NotificationSchema } from "src/domain/entities";
-import { FlagModule } from "src/application/services";
 
 import { UserControllerV1 } from "../controllers";
 import { AdminLogRepository, NotificationRepository, RedisRepository, SessionRepository, SlackRepository, StorageRepository, UserRepository } from "../repositories";
@@ -19,7 +17,6 @@ import { BcryptService } from "../config";
       { name: Entity.AdminLog, schema: AdminLogSchema },
       { name: Entity.Notification, schema: NotificationSchema },
     ]),
-    FlagModule,
   ],
   controllers: [UserControllerV1],
   providers: [
